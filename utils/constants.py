9c7ffe74a3fb27b72850ffabfba84ea316811b05
BrendanWallaceNash98 <brendan.wallacenash@connect.qut.edu.au>
2024-09-07 14:38:14 +1000

    Add config.conf to .gitignore

--- utils/constants.py
+++ utils/constants.py
@@ -18,6 +18,11 @@ DATABASE_PORT = parser.get('database', 'database_port')
 DATABASE_USER = parser.get('database', 'database_user')
 DATABASE_PASSWORD = parser.get('database', 'database_password')
 
+AWS_ACCESS_KEY_ID = parser.get('aws', 'aws_access_key_id')
+AWS_ACCESS_KEY = parser.get('aws', 'aws_secret_access_key')
+AWS_REGION = parser.get('aws', 'aws_region')
+AWS_BUCKET_NAME = parser.get('aws', 'aws_bucket_name')
+
 INPUT_PATH = parser.get('file_paths', 'input_path')
 OUTPUT_PATH = parser.get('file_paths', 'output_path')
 
