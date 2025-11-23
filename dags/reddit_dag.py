4be2d539744d2996f5a9169e5fd83586affcf09b
BrendanWallaceNash98 <brendan.wallacenash@connect.qut.edu.au>
2025-11-23 19:24:39 +1000

    started work on the fake data generator

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -6,7 +6,7 @@ import sys
 from airflow.operators.python import PythonOperator
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+from pipelines.aws_s3_pipeline import upload_s3_pipeline
 from pipelines.reddit_pipeline import reddit_pipeline
 
 default_args = {
@@ -38,3 +38,12 @@ extract = PythonOperator(
     },
     dag=dag
 )
+
+
+upload_s3 = PythonOperator(
+    task_id='s3_upload',
+    python_callable= upload_s3_pipeline,
+    dag=dag
+)
+
+extract >> upload_s3
--- utils/constants.py
+++ utils/constants.py
@@ -29,17 +29,13 @@ OUTPUT_PATH = parser.get('file_paths', 'output_path')
 POST_FIELDS = (
     'id',
     'title',
-    'selftext',
     'score',
     'num_comments',
     'author',
     'created_utc',
     'url',
-    'upvote_ratio',
     'over_18',
     'edited',
     'spoiler',
-    'stickied',
-
-
+    'stickied'
 )
